8f84820f4a6842aa844b813232ff15998b6524cf
Stefan Kondring <kondring.s@gmx.de>
2022-10-18 11:19:27 +0200

    add header, card, navbar with simple html/css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <header>
+        <h1>React and Morty</h1>
+      </header>
+      <div className="list">
+        <article className="card">
+          <img
+            src="https://rickandmortyapi.com/api/character/avatar/2.jpeg"
+            alt="character"
+          />
+          <p>Morty Smith</p>
+          <button>Show more</button>
+        </article>
+      </div>
+      <nav>
+        <div className="icon"></div>
+        <div className="icon"></div>
+        <div className="icon"></div>
+        <div className="icon"></div>
+      </nav>
+    </div>
+  );
 }
 
 export default App;
