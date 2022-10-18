970c00d78b10c98691b44e0c1543afe74db8d65a
Stefan Kondring <kondring.s@gmx.de>
2022-10-18 13:05:17 +0200

    add fetchData-function
    - try/catch
    - save fetched data in state 'cardArray'
    - display characternames

--- src/App.js
+++ src/App.js
@@ -2,13 +2,37 @@ import "./App.css";
 import Header from "./components/Header";
 import Card from "./components/Card";
 import NavBar from "./components/NavBar";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [cardArray, setCardArray] = useState([]);
+
+  useEffect(() => {
+    fetchData(apiURL);
+  }, []);
+
+  const apiURL = "https://rickandmortyapi.com/api/character";
+
+  async function fetchData(URL) {
+    try {
+      const response = await fetch(apiURL);
+      const data = await response.json();
+      console.log(data);
+      setCardArray(data.results);
+      console.log(cardArray);
+    } catch (error) {
+      console.error("Something went wrong: " + error.message);
+    }
+  }
+
   return (
     <div className="App">
       <Header />
       <div className="list">
         <Card />
+        {cardArray.map((result) => {
+          return <p>{result.name}</p>;
+        })}
       </div>
       <NavBar />
     </div>
