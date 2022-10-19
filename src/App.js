724634a6bf61be7fd4a01a5967572a9662cdf60c
Stefan Kondring <kondring.s@gmx.de>
2022-10-19 11:29:59 +0200

    add cards to favorites
    - add FavButton to Cards
    - add handleAddFunction
    - add UseContext to hand over the handleAddFunction

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import { useEffect, useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import CardDetails from "./pages/CardDetails";
 import Layout from "./components/Layout";
+import { UserContext } from "./util/UserContext";
 
 function App() {
   const [cardArray, setCardArray] = useState([]);
+  const [favArray, setFavArray] = useState([]);
 
   useEffect(() => {
     const apiURL = "https://rickandmortyapi.com/api/character";
@@ -23,20 +25,31 @@ function App() {
     fetchData(apiURL);
   }, []);
 
+  function handleAddFav(id, card) {
+    let check = favArray.filter((item) => item.id === id);
+    if (check.length > 0) {
+      return;
+    } else {
+      setFavArray([card, ...favArray]);
+    }
+  }
+
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Home cardArray={cardArray} />} />
-        <Route
-          path="details/:id"
-          element={<CardDetails cardArray={cardArray} />}
-        />
-        <Route
-          path="/*"
-          element={<h1>Diese Seite existiert leider nicht.</h1>}
-        />
-      </Route>
-    </Routes>
+    <UserContext.Provider value={{ cardArray, handleAddFav }}>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home cardArray={cardArray} />} />
+          <Route
+            path="/details/:id"
+            element={<CardDetails cardArray={cardArray} />}
+          />
+          <Route
+            path="/*"
+            element={<h1>Diese Seite existiert leider nicht.</h1>}
+          />
+        </Route>
+      </Routes>
+    </UserContext.Provider>
   );
 }
 
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,13 @@
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
+import { UserContext } from "../util/UserContext";
+import { useContext } from "react";
 
 export default function Card({ cardData }) {
   const { id, name, image } = cardData;
+  const { handleAddFav } = useContext(UserContext);
   const navigate = useNavigate();
+
   return (
     <CharacterCard key={id}>
       <img src={image} alt="character" />
@@ -11,11 +15,13 @@ export default function Card({ cardData }) {
       <StyledButton onClick={() => navigate(`/details/${id}`)}>
         Show more
       </StyledButton>
+      <FavButton onClick={() => handleAddFav(id, cardData)}></FavButton>
     </CharacterCard>
   );
 }
 
 export const CharacterCard = styled.article`
+  position: relative;
   padding: 20px;
   margin: 20px auto;
   border: 3px solid black;
@@ -30,6 +36,22 @@ export const CharacterCard = styled.article`
 const StyledButton = styled.button`
   margin-bottom: 20px;
   padding: 5px;
+`;
+
+const FavButton = styled.button`
+  position: absolute;
+  top: -20px;
+  right: -10px;
+  border: 1px solid black;
+  background-color: white;
+  border-radius: 50%;
+  width: 50px;
+  height: 50px;
+
+  &:hover {
+    background-color: lightgrey;
+    cursor: pointer;
+  }
 `;
 
 export const Name = styled.p`
